d9950ec634bbbc29a6390538ad0b36dbf9f1b503
Yong Joo Lim <yongjoolim@Yongui-MacBook-Pro.local>
2016-10-11 12:56:38 +0900

    삭제기능구현완료

--- mysite_homework/polls/urls.py
+++ mysite_homework/polls/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^$', views.index, name='return_mainpage'),
     url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
     url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
--- mysite_homework/polls/views.py
+++ mysite_homework/polls/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect, HttpResponse
 from .models import Question, Choice
 from django.urls import reverse
 from django.utils import timezone
-from django.views import generic
+
 
 
 # class IndexView(generic.ListView):
@@ -77,36 +77,35 @@ def add_choice(request, question_id):
             return HttpResponse('choice_ name is required')
         elif len(add_choice_name) < 1:
             return HttpResponse('choice_ name is too short')
-    except (KeyError):
-        raise HttpResponse('choice_name kye does not exist')
+    except(KeyError):
+        raise HttpResponse('choice_name key does not exist')
     question.choice_set.create(choice_text=add_choice_name)
     redirect_url = reverse('polls:detail', args=(question_id,))
     return HttpResponseRedirect(redirect_url)
 
 
 def choice_delete_question(request):
-
     question_lists = Question.objects.all()
     context = {
         'question_lists': question_lists,
     }
+    return_value = request.POST.get('return')
     choice_question = request.POST.get('question_id')
-    if choice_question is not None :
+
+    if choice_question is not None:
         print("choice question id: %s" % choice_question)
         delete_question = Question.objects.get(id=choice_question)
         delete_question.delete()
-
+        print("delete complete")
+        if return_value is not None:
+            return render(request, 'choice_delete_question.html', context)
+        else:
+            return render(request, 'choice_delete_question.html', context)
     else:
         print("ELSE구문탔음")
-        # return_yes = request.POST.get('return')
-        # if return_yes is None:
-        #     print("return : %s" %return_yes)
-        #     return render(request, 'choice_delete_question.html', context)
-        # else:
-        #     print("ELSE-ELSE구문탐")
-        return render(request, 'choice_delete_question.html', context)
-
-
-    print("삭제끝")
-    return render(request, 'choice_delete_question.html', context)
+        if return_value is not None:
+            index_url = reverse('polls:index')
+            return HttpResponseRedirect(index_url)
+        else:
+            return render(request, 'choice_delete_question.html', context)
 
